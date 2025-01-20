ea119a0046cc2c8069b4fd99bd2c7734fd17ed15
Flavio Filho <flaviofilholm10@gmail.com>
2025-01-20 16:12:02 -0300

    update: changing the data models

--- src/models/sensorModel.js
+++ src/models/sensorModel.js
@@ -1,12 +1,41 @@
 const pool = require('../config/database');
 
-const insertData = async (sensorId, location, temperature, humidity) => {
+const insertData = async (data) => {
   const query = `
-    INSERT INTO sensor_data (sensor_id, location, temperature, humidity, timestamp) 
-    VALUES ($1, $2, $3, $4, NOW())
+    INSERT INTO sensor_data (
+      data,
+      hora,
+      planta,
+      sensor,
+      temperatura_ar,
+      umidade_ar,
+      umidade_solo
+    ) VALUES ($1, $2, $3, $4, $5, $6, $7)
   `;
+  
   try {
-    await pool.query(query, [sensorId, location, temperature, humidity]);
+    for (const entry of data) {
+      const {
+        Data,
+        Hora,
+        Planta,
+        Sensor,
+        "Temperatura_Ar (°C)": TemperaturaAr,
+        "Umidade_Ar (%)": UmidadeAr,
+        "Umidade_Solo (%)": UmidadeSolo
+      } = entry;
+
+      await pool.query(query, [
+        Data,
+        Hora,
+        Planta,
+        Sensor,
+        TemperaturaAr,
+        UmidadeAr,
+        UmidadeSolo
+      ]);
+    }
+
     console.log('Dados inseridos com sucesso!');
   } catch (error) {
     console.error('Erro ao inserir dados no banco:', error);
